6ce4c2b3d609923216cb5e07c1adc90a24f3bbe0
cerciec <ccolem85@students.kennesaw.edu>
2025-11-19 17:52:52 -0500

    Modify cat breeds and add color patterns
    Updated COMMON_BREEDS list and added COLOR_PATTERNS.

--- SRC/cat_profile.py
+++ SRC/cat_profile.py
@@ -1,4 +1,14 @@
 # cat_profile.py
 # common cat breeds
-COMMON_BREEDS = ["Domestic Shorthair", "American Shorthair", "Domestic Longhair", "Siamese", "Ragdoll", "Maine Coon", "Other / Mixed / Unknown"]
-EXOTIC_BREEDS = ["Abyssinian", "Bengal", "Bombay", "British Shorthair", "Devon Rex", "Exotic Shorthair", "Persian", "Russian Blue", "Scottish Fold", "Siberian", "Sphynx", "Other / Mixed / Unknown"
+COMMON_BREEDS = ["Domestic Shorthair", "American Shorthair", "Domestic Longhair", "Siamese", "Ragdoll", "Maine Coon", "I don't see my cat's breed (type my own)"]
+
+#Common cat color/pattern categories
+COLOR_PATTERNS = ["Tuxedo", "Tabby", "Calico", "Tortie", "Solid", "Bi-Color", "type my own"]
+
+def choose_with_custom(options, prompt_text, custom_default_label):
+
+print(f"\n{prompt_text}")
+for i, item in enumerate(options, start=1):
+  print(f"{i}. {item}")
+
+choice = input("\nEnter the number of your choice: ").strip()
